92d6cf31cfe767a45967ebd298a5e2d6eeb13ab9
jasmine saad <jasmineysaad@gmail.com>
2021-02-15 18:21:09 -0500

    First Commit
    )

---
+++ Lab11/Lab11/Movie.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    class Movie
+    {
+		public Movie(string title, string category)
+		{
+		_title = title;
+		_category = category;
+		}
+		public Movie()
+		{
+
+		}
+	private string _category;
+	private string _title;
+		public  void Animated()
+		{
+
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if(movie._category == "Animated")
+                {
+					Console.WriteLine(movie._title);
+                }
+			}
+		}
+		public void Scifi()
+		{
+
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Scifi")
+				{
+					Console.WriteLine(movie._title);
+				}
+			}
+		}
+		public void Drama()
+		{
+
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Drama")
+				{
+					Console.WriteLine(movie._title);
+				}
+			}
+		}
+		public void Horror()
+		{
+
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Horror")
+				{
+					Console.WriteLine(movie._title);
+				}
+			}
+		}
+		private  List<Movie> InitializeList()
+		{
+			List<Movie> movies = new List<Movie>();
+			movies.Add(new Movie("Terminator", "Scifi"));
+			movies.Add(new Movie("2012", "Scifi"));
+			movies.Add(new Movie("Alien", "Scifi"));
+			movies.Add(new Movie("27 Dresses", "Drama"));
+			movies.Add(new Movie("National Treasure", "Drama"));
+			movies.Add(new Movie("The Little Mermaid", "Animated"));
+			movies.Add(new Movie("The Fox & The Hound", "Animated"));
+			movies.Add(new Movie("Chicken Little", "Animated"));
+			movies.Add(new Movie("Little Women", "Drama"));
+			movies.Add(new Movie("Sinister", "Horror"));
+
+			return movies;
+		}
+	}
+
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resp = true;
+            Console.WriteLine("Welcome to the Movie List Application!\nThere are 10 movies in this list.");
+            while(resp == true)
+            {
+                Console.WriteLine("The category choices are Animated, Horror, Scifi, or Drama.Please pick one of the 4 choices and enter it below:");
+                string response = Console.ReadLine();
+                Movie movie = new Movie();
+
+                if (response == "Animated")
+                {
+                    movie.Animated();
+                }
+                if (response == "SciFi")
+                {
+                    movie.Scifi();
+                }
+                if (response == "Horror")
+                {
+                    movie.Horror();
+                }
+                if (response == "Drama")
+                {
+                    movie.Drama();
+                }
+
+                Console.WriteLine("Do you wish to continue? (y/n)");
+                string respstr = Console.ReadLine();
+                if(respstr =="y")
+                {
+                    resp = true;
+                }
+                else
+                {
+                    resp = false;
+                }
+
+            }
+
+
+        }
+    }
+}
